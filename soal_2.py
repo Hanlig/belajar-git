80306fd60865a892777f1986244f997be38f984b
Hanlig <117884462+Hanlig@users.noreply.github.com>
2022-11-24 13:01:56 +0700

    Add files via upload

---
+++ soal_2.py
@@ -0,0 +1,7 @@
+a = int(input("masukan bilangan bulat pertama:"))
+b = int(input("masukan bilangan bulat kedua:"))
+c = int(input("masukan bilangan bulat ketiga:"))
+if a*a + b*b == c*c:
+    print ("merupakan pythagoras")
+else:
+    print ("bukan merupakan pythagoras")
